7fdc6c450e294e9f687acaa6465c17d50c1531c2
Shambhu Kumar <shambhu384@gmail.com>
2018-05-04 03:37:43 +0530

    Meeting event subscriber added

---
+++ src/EventSubscriber/MeetingRegisteredSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Event\MeetingRegisteredEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+
+class MeetingRegisteredSubscriber implements EventSubscriberInterface
+{
+    use LoggerAwareTrait;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->setLogger($logger);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        // return the subscribed events, their methods and priorities
+        return array(
+            'meeting.registered' => array(
+                array('processMeeting', 10),
+                array('logMeeting', 0),
+                array('notifyMeeting', -10),
+            )
+        );
+    }
+
+    public function processMeeting(MeetingRegisteredEvent $event)
+    {
+        $this->logger->info(sprintf('Metting initilise : %s', $event->getMeeting()->getName()));
+    }
+
+    public function logMeeting(MeetingRegisteredEvent $event)
+    {
+        $this->logger->info(sprintf('Meeting created: %s', $event->getMeeting()->getName()));
+    }
+
+    public function notifyMeeting(MeetingRegisteredEvent $event)
+    {
+        $this->logger->info(sprintf('Notify users about new : %s', $event->getMeeting()->getName()));
+    }
+}
+
