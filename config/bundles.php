11623f2162350dfa44349c190aff119b4de76e22
Shambhu Kumar <shambhu384@gmail.com>
2018-05-04 03:29:22 +0530

    Mono logger bundle added

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
