dda7aff87bd21c04a8059a4197c275f8a8c0badc
Shambhu Kumar <shambhu.kumar@techaspect.com>
2018-05-05 03:12:17 +0530

    User device repo

---
+++ src/Repository/UserDeviceRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\UserDevice;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method UserDevice|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UserDevice|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UserDevice[]    findAll()
+ * @method UserDevice[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserDeviceRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, UserDevice::class);
+    }
+
+//    /**
+//     * @return UserDevice[] Returns an array of UserDevice objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?UserDevice
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
