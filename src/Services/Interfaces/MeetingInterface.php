7b2c212edeefe740dbd36c3d0e3b2eb996103d07
Shambhu Kumar <shambhu384@gmail.com>
2018-05-05 22:29:49 +0530

    Meeting interface added Flexible services

---
+++ src/Services/Interfaces/MeetingInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+use App\Entity\Meeting;
+
+interface MeetingInterface
+{
+    public function getMeeting(int $id): Meeting;
+    public function getMeetings(array $params): array;
+    public function addMeeting(array $params): bool;
+    public function updateMeeting(int $id, array $data): Meeting;
+    public function deleteMeeting(int $id): bool;
+}
