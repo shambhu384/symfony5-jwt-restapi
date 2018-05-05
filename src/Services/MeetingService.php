f197de123efba4d017f9b8e7e5d09879e6e4fadd
Shambhu Kumar <shambhu384@gmail.com>
2018-05-05 22:30:29 +0530

    Meeting service added

---
+++ src/Services/MeetingService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Services;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\MeetingRepository;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use App\Entity\User;
+use App\Services\Interfaces\MeetingInterface;
+use App\Entity\Meeting;
+
+/**
+ * Class MeetingService
+ *
+ * @package App\Serivces
+ */
+class MeetingService implements MeetingInterface
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var MeetingRepository
+     */
+    private $meetingRepository;
+
+    /**
+     * @var User|null|object
+     */
+    private $user;
+
+    /**
+     * MeetingService constructor
+     *
+     * @param EntityManagerInterface $entityManager
+     * @param MeetingService $repository
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(EntityManagerInterface $entityManager, MeetingRepository $repository, TokenStorageInterface $tokenStorage)
+    {
+        $this->em = $entityManager;
+        $this->meetingRepository = $repository;
+            
+        //$this->user = $entityManager->getRepository(User::class)->findOneBy(['username' => $tokenStorage->getToken()->getUser()->getUsername()]);
+    }
+
+    /**
+     * @return Meeting
+     */
+    public function getMeeting(int $id): Meeting
+    {
+        return new Meeting();
+    }
+
+    /**
+     * @return Meeting[]
+     */
+    public function getMeetings(array $params): array
+    {
+        return $this->meetingRepository->findAll();
+    }
+
+    /**
+     *
+     */
+    public function addMeeting(array $data): bool
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function updateMeeting(int $id, array $data): Meeting
+    {
+
+    }
+
+    /**
+     * @param array
+     */
+    public function deleteMeeting(int $id): bool
+    {
+        return false;
+    }
+}
