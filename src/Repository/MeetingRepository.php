df331d6d6e3bd0506e6a3cb800a42715b94d4306
Shambhu Kumar <shambhu384@gmail.com>
2018-05-05 22:27:26 +0530

    Meeting repo added

---
+++ src/Repository/MeetingRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Meeting;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Meeting|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Meeting|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Meeting[]    findAll()
+ * @method Meeting[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MeetingRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Meeting::class);
+    }
+
+//    /**
+//     * @return Meeting[] Returns an array of Meeting objects
+//     */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Meeting
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
