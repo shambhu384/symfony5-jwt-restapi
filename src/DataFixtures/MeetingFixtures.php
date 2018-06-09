99899afe302975eab9b74c9b86521d17bae432e9
Shambhu Kumar <shambhu384@gmail.com>
2018-06-09 15:15:36 +0530

    Added Data fixtures

---
+++ src/DataFixtures/MeetingFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\Entity\Meeting;
+
+
+class MeetingFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $meeting = new Meeting();
+        $meeting->setName('Symfony apps');
+        $meeting->setDescription('Symfony apps description');
+        $meeting->setDateTime(new \DateTime('now'));
+        // this reference returns the User object created in UserFixtures
+        $meeting->setUser($this->getReference(UserFixtures::USER_REFERENCE));
+        $manager->persist($meeting);
+
+        $manager->flush();
+
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+        );
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+
+    private $encoder;
+
+    public const USER_REFERENCE = 'admin-user';
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+            $this->encoder = $encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('shambhu384@gmail.com');
+        $user->setFullname('Shambhu Kumar');
+
+        $password = $this->encoder->encodePassword($user, 'pass_1234');
+        $user->setPassword($password);
+        $manager->persist($user);
+
+        $manager->flush();
+        // other fixtures can get this object using the UserFixtures::ADMIN_USER_REFERENCE constant
+        $this->addReference(self::USER_REFERENCE, $user);
+    }
+}
